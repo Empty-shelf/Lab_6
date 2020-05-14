36d7a40b1b495332f49c690a0311e8b3323e3ea7
Ekaterina <ekaterina.2001@live.com>
2020-05-14 03:05:45 +0300

    .

--- Server/src/Work/ServerMain.java
+++ Server/src/Work/ServerMain.java
@@ -4,7 +4,7 @@ import Common.ExecuteManager;
 
 public class ServerMain {
     public static void main(String[] args) {
-        Commander commander = new Commander(ExecuteManager.getInstance("file_user.csv"), 1234);
+        Commander commander = new Commander(ExecuteManager.getInstance(System.getenv("Coll_Path")), 1234);
         while (true) {
             commander.start();
         }
